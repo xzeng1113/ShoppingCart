c4fb900228bcd4a883e95c0ecd805e343e4d4f41
Xiaoxi Zeng <43712149+xzeng1113@users.noreply.github.com>
2021-04-09 18:29:22 -0700

    Add files via upload

--- LeapcodeShoppingCart/src/main/java/com/Leapcode/LeapcodeShoppingCart/controllers/AdminPagesController.java
+++ LeapcodeShoppingCart/src/main/java/com/Leapcode/LeapcodeShoppingCart/controllers/AdminPagesController.java
@@ -5,26 +5,95 @@ import com.Leapcode.LeapcodeShoppingCart.models.data.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
+
     @Autowired
     private PageRepository pageRepo;
+
     @GetMapping
-    public String index(Model model){
+    public String index(Model model) {
         List<Page> pages = pageRepo.findAll();
         model.addAttribute("pages", pages);
-        return "/admin/pages/index";
+        return "admin/pages/index";
     }
 
     @GetMapping("/add")
-    public String add(Model model){
+    public String add(Model model) {
         model.addAttribute("page", new Page());
         return "admin/pages/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model){
+        if (bindingResult.hasErrors()) {
+            return "admin/pages/add";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+        String casing = page.getCasing() == "" ? page.getTitle().toLowerCase().replace(" ", "-") :
+                page.getCasing().toLowerCase().replace(" ", "-");
+        Page casingExists = pageRepo.findByCasing(casing);
+        if (casingExists != null){
+            redirectAttributes.addFlashAttribute("message", "Casing exists, choose another name");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+        } else {
+            page.setCasing(casing);
+            page.setSorting(100);
+            pageRepo.save(page);
+        }
+        return "redirect:/admin/pages/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+        Page page = pageRepo.getOne(id);
+        model.addAttribute("page", page);
+        return "admin/pages/edit";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Valid Page page, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "admin/pages/edit";
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Page edited");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+        String casing = page.getCasing() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getCasing().toLowerCase().replace(" ", "-");
+
+        Page casingExists = pageRepo.findByCasing(page.getId(), casing);
+
+        if (casingExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("page", page);
+        } else {
+            page.setCasing(casing);
+
+            pageRepo.save(page);
+        }
+        return "redirect:/admin/pages/edit/" + page.getId();
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id, RedirectAttributes redirectAttributes) {
+        pageRepo.deleteById(id);
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+        return "redirect:/admin/pages";
     }
 }
--- LeapcodeShoppingCart/src/main/java/com/Leapcode/LeapcodeShoppingCart/models/PageRepository.java
+++ LeapcodeShoppingCart/src/main/java/com/Leapcode/LeapcodeShoppingCart/models/PageRepository.java
@@ -2,8 +2,11 @@ package com.Leapcode.LeapcodeShoppingCart.models;
 
 import com.Leapcode.LeapcodeShoppingCart.models.data.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
+    Page findByCasing(String casing);
 
-
+    @Query("SELECT p FROM Page p WHERE p.id != :id and p.casing != :casing")
+    Page findByCasing(int id, String casing);
 }
